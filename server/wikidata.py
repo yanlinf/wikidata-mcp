2cd0dafa4daccc056ce64a7364e420264437f83e
Yanlin <yanlinf23@gmail.com>
2025-03-16 15:46:55 -0700

    Add tool lookup_item

--- server/wikidata.py
+++ server/wikidata.py
@@ -1,5 +1,6 @@
 from typing import Any
 from mcp.server.fastmcp import FastMCP
+import asyncio
 import aiohttp
 
 # Initialize FastMCP server
@@ -15,10 +16,9 @@ async def run_sparql(query: str) -> dict:
     Returns:
         dict: The JSON response from the Wikidata SPARQL endpoint
     """
-    # return {"result": "test"}
     # Define the endpoint URL for Wikidata's SPARQL endpoint
     endpoint_url = "https://query.wikidata.org/sparql"
-    
+
     # Set up the query parameters
     params = {
         'query': query,
@@ -39,6 +39,39 @@ async def run_sparql(query: str) -> dict:
                 print(f"Error: {response.status}")
                 return None
 
+
+@mcp.tool()
+async def lookup_item(item: str) -> str:
+    """Lookup a Wikidata item to get the identifier.
+
+    Args:
+        item: Wikidata item name
+
+    Returns:
+        str: Search results for the item
+    """
+    # curl "https://www.wikidata.org/w/api.php?action=wbsearchentities&search=Albert%20Einstein&language=en&format=json"
+
+    # Define the endpoint URL for Wikidata's API
+    endpoint_url = "https://www.wikidata.org/w/api.php"
+    
+    # Set up the query parameters
+    params = {
+        'action': 'wbsearchentities',
+        'search': item,
+        'language': 'en',
+        'format': 'json'
+    }
+
+    # Use aiohttp to send the request asynchronously
+    async with aiohttp.ClientSession() as session:
+        async with session.get(endpoint_url, params=params) as response:
+            if response.status == 200:
+                return await response.json()  # Return the JSON response
+            else:
+                print(f"Error: {response.status}")
+                return None
+
 if __name__ == "__main__":
     # sparql = """
     # SELECT ?item ?itemLabel WHERE {
@@ -47,6 +80,10 @@ if __name__ == "__main__":
     # } LIMIT 10
     # """
     # response = asyncio.run(run_sparql(sparql))
+    # print(response)
+
+    # item = "Albert Einstein"
+    # response = asyncio.run(lookup_item(item))
     # print(response)
 
     # Initialize and run the server
