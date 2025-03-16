80520144f44c507095268e14ea2dc678bcb1c769
yongkangzhao <cookieyong@gmail.com>
2025-03-16 15:37:40 -0700

    add client

--- client.py
+++ client.py
@@ -97,7 +97,7 @@ class MCPClient:
 
                 # Get next response from Claude
                 response = self.anthropic.messages.create(
-                    model="claude-3-5-sonnet-20241022",
+                    model="claude-3-7-sonnet-20250219",
                     max_tokens=1000,
                     messages=messages,
                 )
